bed631d6e9a3659148d073e0e84cd78d742461f0
igor4022 <tic80padraolua@gmail.com>
2025-07-22 19:48:01 -0300

    se alguem puder me ajudar, valeu

--- arquivo.js
+++ arquivo.js
@@ -1,7 +1,34 @@
+let calculo = document.getElementById('calcular')
 
+function resull(){
+let resultado = document.getElementById('resultado')
+        if(resultado){
 
+        resultado.innerHTML = eval('resultado')
+
+}
+}
 function botom(num)
 
     {
-        document.getElementById('calcular').innerHTML = num;
+        calculo.innerHTML = num + calculo.innerHTML
     }
+
+function vezes(ves)
+
+   {
+    
+        calculo.innerHTML = ves + calculo.innerHTML
+   }
+
+function mais(mas){
+    calculo.innerHTML = mas + calculo.innerHTML
+}
+
+function menos(nos){
+    calculo.innerHTML = nos + calculo.innerHTML
+}
+
+function divisao(vis){
+    calculo.innerHTML = vis + calculo.innerHTML
+}
